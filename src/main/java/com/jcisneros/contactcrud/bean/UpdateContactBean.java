fd19d17a74cd40ae1597e2019905f2fde4595dba
José Cisneros <josecisneros@Joses-MacBook-Pro.local>
2025-02-16 16:49:32 -0600

    Basic cloudinary photo upload

--- src/main/java/com/jcisneros/contactcrud/bean/UpdateContactBean.java
+++ src/main/java/com/jcisneros/contactcrud/bean/UpdateContactBean.java
@@ -1,6 +1,10 @@
 package com.jcisneros.contactcrud.bean;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.annotation.PostConstruct;
@@ -9,6 +13,7 @@ import javax.faces.component.FacesComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 
+import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.file.UploadedFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,14 +22,18 @@ import org.springframework.web.context.annotation.SessionScope;
 
 import com.jcisneros.contactcrud.contact.Contact;
 import com.jcisneros.contactcrud.contact.ContactRepository;
+import com.jcisneros.contactcrud.services.PhotoService;
 
 @Component("updateContactBean")
 @ViewScoped
 public class UpdateContactBean implements Serializable {
 
 
-@Autowired
-ContactRepository contactRepository;
+    @Autowired
+    ContactRepository contactRepository;
+
+    @Autowired
+    PhotoService photoService;
 
     Contact contact;
     private UploadedFile photoFile;
@@ -51,8 +60,40 @@ ContactRepository contactRepository;
     }
     
     public String save() {
+
+        // if(this.photoFile != null) {
+        //     try {
+        //         InputStream photoStream = new FileInputStream(this.photoFile.getFileName());
+        //         Map<String,String> result;
+        //         result = photoService.uploadImage(photoStream);
+
+        //         if(result.get("url") != null) {
+        //             this.contact.setImageUrl(result.get("url"));
+        //         }
+        //     } catch(IOException err) {
+        //         System.out.println("Error getting photo input stream: " + err);
+        //     }
+            
+        // }
+
         contactRepository.save(this.contact);
         return "/views/contact-list.xhtml?faces-redirect=true";
+    }
+
+    public void handleFileUpload(FileUploadEvent event) {
+        this.photoFile = event.getFile();
+        if(this.photoFile != null) {
+            
+                byte[] photoStream = this.photoFile.getContent();
+                Map<String,String> result;
+                result = photoService.uploadImage(photoStream);
+
+                if(result.get("url") != null) {
+                    this.contact.setImageUrl(result.get("url"));
+                }
+            
+            
+        }
     }
     
     public Contact getContact() {
---
+++ src/main/java/com/jcisneros/contactcrud/services/PhotoService.java
@@ -0,0 +1,44 @@
+package com.jcisneros.contactcrud.services;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import org.primefaces.model.file.UploadedFile;
+import org.springframework.stereotype.Service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Transformation;
+import com.cloudinary.utils.ObjectUtils;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+
+@Service
+public class PhotoService {
+    private final Dotenv dotenv = Dotenv.load();
+    
+    private final Cloudinary cloudinary = new Cloudinary(dotenv.get("CLOUDINARY_URL"));
+    
+    public Map uploadImage(byte[] file) {
+        
+        Map params1 = ObjectUtils.asMap(
+            "use_filename", true,
+            "unique_filename", false,
+            "overwrite", true,
+            "folder", "contact-crud",
+            "transformation", new Transformation().width(500).height(500).crop("fill").gravity("face")
+            );
+            
+        try {
+            Map result = cloudinary.uploader().upload(file, params1);
+            return result;
+            
+        } catch(IOException e) {
+            System.out.println("Error when uploading image." + e) ;
+            return ObjectUtils.emptyMap();
+        }
+    }
+        
+}
+    
